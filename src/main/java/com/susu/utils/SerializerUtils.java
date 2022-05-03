20e2cc1ea12d6e407bdf7bee65c0f0162702fba5
fxbsujay <fxbsujay@gmail.com>
2022-05-03 23:59:33 +0800

    进制运算

---
+++ src/main/java/com/susu/utils/SerializerUtils.java
@@ -0,0 +1,9 @@
+package com.susu.utils;
+
+/**
+ * @Author sujay
+ * @Description 描述
+ * @Date 23:29 2022/5/2
+ */
+public class SerializerUtils {
+}
--- src/main/java/com/susu/utils/Test.java
+++ src/main/java/com/susu/utils/Test.java
@@ -1,5 +1,13 @@
 package com.susu.utils;
 
+import com.susu.serializer.Bar;
+import com.susu.serializer.Baz;
+import com.susu.serializer.Foo;
+import com.susu.taira.Taira;
+import com.susu.taira.TairaData;
+import java.nio.ByteOrder;
+import java.util.Arrays;
+
 /**
  * <p>Description:测试类</p>
  * @author sujay
@@ -9,12 +17,60 @@ public class Test {
 
 
     public static void main(String[] args) {
-        String a = "3";
-        byte[] bytes = a.getBytes();
-        System.out.println(bytes[0]);
-        System.out.println(0xFF);
-        System.out.println(0xFF & bytes[0]);
-        System.out.println(Integer.toHexString(0xFF & bytes[0]));
+        Taira.DEBUG = true;
+
+        // Init object
+        Foo foo = new Foo();
+        foo.setByteField((byte) 2);
+        foo.setIntField(103);
+        foo.setCharField('$');
+        foo.setDoubleField(123.21);
+        foo.setBytesField(new byte[] { 11, 22, 33, 44 });
+        foo.setStringField("world");
+        foo.setRemainsStringField("Hi Ro Hi Ro Ma U Hi Ka Ri No Na Ka");
+
+        Bar bar = new Bar();
+        bar.setFloatVal(123.2f);
+        bar.setShortVal((short) 11);
+        bar.setLongVal(1242354L);
+        bar.setBooleanVal(true);
+
+        Baz[] bazArray = new Baz[3];
+        bazArray[0] = new Baz(1);
+        bazArray[1] = new Baz(3);
+        bazArray[2] = new Baz(5);
+
+        bar.setInnerArrayVal(bazArray);
+        foo.setBarField(bar);
+        foo.setIntListField(Arrays.asList(3, 5, 9));
+
+        System.out.println("fooObject: " + foo.toString());
+
+        // Taira serialize
+        taira(foo);
+
+    }
+
+    /**
+     * Use Taira serialize/deserialize 1000 times
+     */
+    private static void taira(TairaData data) {
+        long serializeStart = System.currentTimeMillis();
+        byte[] fooBytes = new byte[0];
+        for (int i = 0; i < 1000; i++) {
+            fooBytes = Taira.DEFAULT.toBytes(data);
+        }
+        System.out.println("Taira serialize x 1000 time cost: " + (System.currentTimeMillis() - serializeStart));
+        System.out.println("Taira serialize data size: " + fooBytes.length);
+
+        Taira deserializeTaira = new Taira(ByteOrder.BIG_ENDIAN);
+        long deserializeStart = System.currentTimeMillis();
+        Foo foo = null;
+        for (int i = 0; i < 1000; i++) {
+            foo = deserializeTaira.fromBytes(fooBytes, Foo.class);
+        }
+        System.out.println("Taira deserialize x 1000 time cost: " + (System.currentTimeMillis() - deserializeStart));
+        System.out.println("Taira deserialize result: " + String.valueOf(foo));
     }
 
 }
